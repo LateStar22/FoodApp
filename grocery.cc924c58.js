ff9828839c08789321c0adc6b6a69a8e745d8c51
Aniket Shukla <aniketshukla42022@gmail.com>
2024-02-22 02:11:22 +0530

    Updates

---
+++ grocery.cc924c58.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire6aae;(0,e.register)("DxNKZ",function(r,a){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),t=()=>/*#__PURE__*/(0,o.jsx)("h1",{children:"Grocery store!!! We have so many child elements under us!!!"})});//# sourceMappingURL=grocery.cc924c58.js.map
+
+//# sourceMappingURL=grocery.cc924c58.js.map
